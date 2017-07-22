3770a4034841b55989815d98b7e5969e842546df
Osshrp <osshrp@gmail.com>
2017-07-22 18:37:43 +0300

    add view user acceptance test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+
+    context 'unauthenticated user tries to get#show' do
+      before { get :show, params: { id: users.first } }
+
+      it 'does not assign requested question to @question' do
+        expect(assigns(:user)).to_not eq users.first
+      end
+
+      it 'does not renders show view' do
+        expect(response).to_not render_template :show
+      end
+    end
+
+    context 'authenticated user tries to get#show' do
+      sign_in_user
+      before { get :show, params: { id: users.first } }
+
+      it 'does not assign requested question to @question' do
+        expect(assigns(:user)).to eq users.first
+      end
+
+      it 'does not renders show view' do
+        expect(response).to render_template :show
+      end
+    end
+  end
 end
